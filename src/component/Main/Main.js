0502b640a9e0859d985b4cd8aef2b04cb93bb5cc
Saurav <sauravkumar00108@gmail.com>
2024-08-26 19:42:32 +0530

    first commit

---
+++ src/component/Main/Main.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className="flex justify-between items-center bg-black text-white h-screen p-8">
+            <div className="flex-1">
+                <img
+                    src="https://via.placeholder.com/500x500"
+                    alt="rabbit r1"
+                    className="w-3/4 mx-auto"
+                />
+            </div>
+
+            <div className="flex-1 text-right mt-3">
+                <div className="text-container text-justify">
+                    <h1 className="text-6xl font-light mb-4 font-serif text-white max-w-[60%]">
+                        MATAER 1
+                    </h1>
+                    <p className="text-2xl font-serif	 mb-4 text-gray-400 max-w-[55%] leading-relaxed">
+                        your smart home companion, Powered by <span className="text-white">AERSENSE <span className="text-red-600 ">™</span></span>
+                    </p>
+                    <p className="text-sm font-semibold text-gray-400 mb-8 mt-8 max-w-[28%]">
+                        ships in November 2024
+                    </p>
+                    <p className="text-3xl mb-2 flex justify-start items-center">
+                    <svg className='inline' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-indian-rupee"><path d="M6 3h12"/><path d="M6 8h12"/><path d="m6 13 8.5 8"/><path d="M6 13h3"/><path d="M9 13c6.667 0 6.667-10 0-10"/></svg>899 <span className="text-sm ml-2">INR</span>
+                    </p>
+                    <p className="text-sm text-gray-400 mb-8">
+                        no subscription
+                    </p>
+                    <button className="bg-orange-500 text-white py-2 px-6 rounded-full flex items-center justify-center">
+                        add to bag
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6 ml-2"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M13 7l5 5m0 0l-5 5m5-5H6"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-black p-4 flex items-center justify-between">
+      <div className="text-white text-3xl font-light italic">
+        Aersense<span className="text-red-600 ">™</span>
+      </div>
+      <div className="flex space-x-16">
+        <a href="#rabbit-r1" className="text-white text-sm">
+          rabbit r1
+        </a>
+        <a href="#rabbit-os" className="text-white text-sm">
+          rabbit os
+        </a>
+        <a href="#research" className="text-white text-sm">
+          research
+        </a>
+        <a href="#newsroom" className="text-white text-sm">
+          newsroom
+        </a>
+        <a href="#support" className="text-white text-sm">
+          support
+        </a>
+      </div>
+      <button className="text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l1.2 6h7.6l1.2-6M7 13h10m0 0H6"
+          />
+        </svg>
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
