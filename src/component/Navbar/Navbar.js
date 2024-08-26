8ebe97c0954dda7653caf9848b5e07ddd47db422
Saurav <sauravkumar00108@gmail.com>
2024-08-26 20:52:37 +0530

    chore: Improve sidebar functionality in Navbar component

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -7,16 +7,20 @@ const Navbar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <nav className="bg-black p-4 flex items-center justify-between">
       <div className="text-white text-3xl font-light italic">
         Aersense<span className="text-red-600 ">™</span>
       </div>
       <div className="hidden md:flex flex-1 justify-center space-x-16">
-        <a href="#rabbit-r1" className="text-white text-sm">
+        <a href="" className="text-white text-sm">
           rabbit r1
         </a>
-        <a href="#rabbit-os" className="text-white text-sm">
+        <a href="" className="text-white text-sm">
           rabbit os
         </a>
         <a href="#research" className="text-white text-sm">
@@ -46,23 +50,29 @@ const Navbar = () => {
         </svg>
       </button>
       {isSidebarOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-75 flex flex-col items-center justify-center space-y-8 md:hidden">
-          <a href="#rabbit-r1" className="text-white text-lg" onClick={toggleSidebar}>
-            rabbit r1
-          </a>
-          <a href="#rabbit-os" className="text-white text-lg" onClick={toggleSidebar}>
-            rabbit os
-          </a>
-          <a href="#research" className="text-white text-lg" onClick={toggleSidebar}>
-            research
-          </a>
-          <a href="#newsroom" className="text-white text-lg" onClick={toggleSidebar}>
-            newsroom
-          </a>
-          <a href="#support" className="text-white text-lg" onClick={toggleSidebar}>
-            support
-          </a>
-        </div>
+        <>
+          <div
+            className="fixed inset-0 bg-black bg-opacity-75 md:hidden"
+            onClick={closeSidebar}
+          ></div>
+          <div className="fixed inset-0 flex flex-col items-center justify-center space-y-8 md:hidden">
+            <a href="" className="text-white text-lg" onClick={closeSidebar}>
+              rabbit r1
+            </a>
+            <a href="" className="text-white text-lg" onClick={closeSidebar}>
+              rabbit os
+            </a>
+            <a href="#research" className="text-white text-lg" onClick={closeSidebar}>
+              research
+            </a>
+            <a href="#newsroom" className="text-white text-lg" onClick={closeSidebar}>
+              newsroom
+            </a>
+            <a href="#support" className="text-white text-lg" onClick={closeSidebar}>
+              support
+            </a>
+          </div>
+        </>
       )}
     </nav>
   );
